78d697074968b30ab3d78a6e9a5928b86a1c6a47
Evdokimov11 <Evdokimov11.1996@gmail.com>
2023-01-20 06:59:22 +0400

    rename variables

--- main.py
+++ main.py
@@ -66,123 +66,123 @@ def print_table(languages_information, title):
 
 def get_hh_information(programming_languages):
 
-    url_hh = 'https://api.hh.ru/vacancies'
+    url = 'https://api.hh.ru/vacancies'
 
-    languages_information_hh = {}
+    languages_information = {}
 
-    salaries_hh = []
+    salaries = []
 
     for programming_language in programming_languages :
     
-        params_hh = {  
+        params = {  
                 'text' : f'Программист {programming_language}',
                 'area' : 1,
                 'date_from': date.today() - relativedelta(months=1)
         }
       
-        response_hh = requests.get(url_hh, params=params_hh)
+        response = requests.get(url, params=params)
       
-        response_hh.raise_for_status()
+        response.raise_for_status()
       
-        response_hh_formatted = response_hh.json()
+        response_formatted = response.json()
       
-        sum_vacancies_hh = response_hh_formatted['found']
+        sum_vacancies = response_formatted['found']
       
-        vacancies_hh = response_hh_formatted['items']
-      
-        salaries_hh.clear()
-      
-        for vacancy_hh in vacancies_hh :
-      
-            expected_salary_hh = predict_rub_salary_hh(vacancy_hh)
-      
-            if expected_salary_hh:
-            
-                salaries_hh.append(expected_salary_hh)
-              
-        if  salaries_hh :
+        vacancies = response_formatted['items']
         
-            average_salary_hh = sum(salaries_hh)/len(salaries_hh)
+        salaries.clear()
+      
+        for vacancy in vacancies :
+      
+            expected_salary = predict_rub_salary_hh(vacancy)
+      
+            if expected_salary:
+            
+                salaries.append(expected_salary)
+              
+        if  salaries:
+        
+            average_salary = sum(salaries)/len(salaries)
       
         else:
       
-            average_salary_hh = 'Вакансий не найдено'
+            average_salary = 'Вакансий не найдено'
         
-        languages_information_hh[programming_language] = { 
-              "vacancies_found": sum_vacancies_hh,
-              "vacancies_processed": len(salaries_hh),
-              "average_salary": average_salary_hh
+        languages_information[programming_language] = { 
+              "vacancies_found": sum_vacancies,
+              "vacancies_processed": len(salaries),
+              "average_salary": average_salary
           }
       
-    return languages_information_hh
+    return languages_information
 
 
-def get_sj_information(programming_languages, key_sj):
+def get_sj_information(programming_languages, api_key):
       
-    url_sj = 'https://api.superjob.ru/2.0/vacancies/'
+    url = 'https://api.superjob.ru/2.0/vacancies/'
     
-    languages_information_sj = {}
+    languages_information = {}
     
-    salaries_sj = []
+    salaries = []
     
-    headers_sj = {
-       'X-Api-App-Id':key_sj, 
+    headers = {
+       'X-Api-App-Id':api_key, 
       }
     
     for programming_language in programming_languages :
     
-        params_sj = {
+        params = {
            'keyword': f'Программист {programming_language}',
            'catalogues':33, 
            'town':4
           }
           
-        response = requests.get(url_sj, headers=headers_sj, params=params_sj)
+        response = requests.get(url, headers=headers, params=params)
         
-        response_formatted_sj = response.json()
+        response_formatted = response.json()
         
-        vacancies_sj = response_formatted_sj['objects']
+        vacancies = response_formatted['objects']
         
-        average_salary_sj = 0
+        average_salary = 0
         
-        salaries_sj.clear()
+        salaries.clear()
       
-        for vacancy_sj in vacancies_sj:
+        for vacancy in vacancies:
     
-            expected_salary_sj = predict_rub_salary_sj(vacancy_sj)
+            expected_salary = predict_rub_salary_sj(vacancy)
         
-            if expected_salary_sj:
+            if expected_salary:
               
-                 salaries_sj.append(expected_salary_sj)
+                 salaries.append(expected_salary)
         
-        if len(salaries_sj):
+        if len(salaries):
         
-            average_salary_sj = sum(salaries_sj) / len(salaries_sj)
+            average_salary = sum(salaries) / len(salaries)
     
         else:
     
-            average_salary_sj = 'Вакансии не найдены'
+            average_salary = 'Вакансии не найдены'
         
-        languages_information_sj[programming_language] = {
-                "average_salary": average_salary_sj,
-                "vacancies_found": response_formatted_sj['total'],
-                "vacancies_processed": len(salaries_sj)
+        languages_information[programming_language] = {
+                "average_salary": average_salary,
+                "vacancies_found": response_formatted['total'],
+                "vacancies_processed": len(salaries)
         }
     
-    return languages_information_sj
+    return languages_information
 
 
 if __name__ == "__main__":
 
   load_dotenv()
 
-  key_sj = os.environ['KEY_SUPER_JOB']
+  api_key_sj = os.environ['API_KEY_SJ']
 
   programming_languages = ['JavaScript','Java','Python','Ruby','PHP','C++','C#', 'C']
 
   languages_information_hh = get_hh_information(programming_languages)
 
-  languages_information_sj = get_sj_information(programming_languages, key_sj)
+  languages_information_sj = get_sj_information(programming_languages, api_key_sj)
 
   print_table(languages_information_hh, 'HeadHunter Moscow')
 
